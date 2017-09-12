7b0c0e7ad0530d14347401f6e25b635005fc1d69
Jiamao Zheng <jiamaozheng@Im-Lab-015.local>
2017-09-12 14:05:33 -0500

    add some comments

--- imlab_encryption_tool.sh
+++ imlab_encryption_tool.sh
@@ -6,26 +6,36 @@
 # save it in a safe place (e.g. ~/.ssh/)
 
 #--------------------------Encryption--------------------------------
-
+echo "\n"
 if [ $3 = "encrypt" ]; then 
+counter=0 
+start=$SECONDS
 find $1 -not -path '*/\.*' -type f \( ! -iname '.*' \) -print0 | while read -d $'\0' file; do
 	if [[ ${file: -4} != ".enc" ]]; then 
-		echo "encrypting $file ...\n"
+		echo "encrypting $file ..."
     	openssl aes-256-cbc -in "$file" -out "$file.enc" -pass file:$2
-    	echo "deleting $file ...\n"
+    	echo "deleting $file ..."
     	rm "$file"
+    	let counter++
+    	end=$SECONDS
+    	echo "$counter files have been encrypted in $((end-start)) seconds \n"
     fi 
 done
 fi 
 
 #------------------------Decryption--------------------------------
-if [ $3 = "decrypt" ]; then 
+if [ $3 = "decrypt" ]; then
+counter=0  
+start=$SECONDS
 find $1 -not -path '*/\.*' -type f \( ! -iname '.*' \) -print0 | while read -d $'\0' file; do
 	if [ ${file: -4} = ".enc" ]; then 
-		echo "decrypting $file ...\n"
+		echo "decrypting $file ..."
 	    openssl aes-256-cbc -d -in "$file" -out "${file/%????}" -pass file:$2
-	    echo "deleting $file ...\n"
+	    echo "deleting $file ..."
 	    rm "$file"
+	    let counter++
+	    end=$SECONDS
+	    echo "$counter files have been decrypted in $((end-start)) seconds\n"
 	fi 
 done
 fi
