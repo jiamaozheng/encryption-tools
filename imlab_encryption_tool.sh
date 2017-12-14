5ce82f6d33c65587e3c229034914489c792b52d3
Jiamao Zheng <jiamaozheng@Im-Lab-015.local>
2017-12-14 16:53:55 -0600

    handling storage space limit condition

--- imlab_encryption_tool.sh
+++ imlab_encryption_tool.sh
@@ -12,77 +12,116 @@ LOG_FILE="$day.txt"
 echo 
 echo  >> $LOG_FILE
 if [ $3 = "encrypt" ]; then 
-echo "---------------------------------- start encrypting files in $1 ----------------------------------"
-echo "---------------------------------- start encrypting files in $1 ----------------------------------" >> $LOG_FILE
+
+log_file="---------------------------------- start encrypting files in $1 ----------------------------------"
+echo $log_file
+echo $log_file >> $LOG_FILE
 echo 
 echo  >> $LOG_FILE
 counter=0 
 start=$SECONDS
+
 find $1 -not -path '*/\.*' -type f \( ! -iname '.*' \) -print0 | while read -d $'\0' file; do
 	if [[ ${file: -4} != ".enc" ]] && [ -f $2 ]; then 
-		echo "encrypting $file ..."
-		echo "encrypting $file ..." >> $LOG_FILE
-	    openssl aes-256-cbc -in "$file" -out "$file.enc" -pass file:$2
-    	echo "deleting $file ..."
-    	echo "deleting $file ..." >> $LOG_FILE
-    	rm "$file"
-    	let counter++
-    	end=$SECONDS
-    	echo "$counter files have been encrypted in $((end-start)) seconds"
-    	echo "$counter files have been encrypted in $((end-start)) seconds" >> $LOG_FILE
-    	echo  
-    	echo  >> $LOG_FILE
+		log_file="encrypting $file ..."
+		echo $log_file
+		echo $log_file >> $LOG_FILE
+
+		if $( openssl aes-256-cbc -in "$file" -out "$file.enc" -pass file:$2 ); then 
+		    openssl aes-256-cbc -in "$file" -out "$file.enc" -pass file:$2
+
+		    log_file="deleting $file ..."
+	    	echo $log_file
+	    	echo $log_file >> $LOG_FILE
+	    	rm "$file"
+	    	let counter++
+	    	end=$SECONDS
+
+	    	log_file="$counter files have been encrypted in $((end-start)) seconds"
+	    	echo $log_file
+	    	echo $log_file >> $LOG_FILE
+	    	echo  
+	    	echo  >> $LOG_FILE
+   		else 
+   			rm "$file.enc"
+
+			log_file="something is wrong!"
+			echo 
+		    echo $log_file
+		    echo 
+	    	echo $log_file >> $LOG_FILE
+	    	break 
+	    fi 	
 	else 
 		if [ ! -f $2 ]; then 
-		    echo "password file doesn't exit ..."
-	    	echo "password file doesn't exit ..." >> $LOG_FILE
+			log_file="password file doesn't exit!"
+		    echo $log_file
+	    	echo $log_file >> $LOG_FILE
+	    	break 
     	fi 
     fi 
 done
-echo "---------------------------------- finish encrypting files in $1 ----------------------------------"
-echo "---------------------------------- finish encrypting files in $1 ----------------------------------" >> $LOG_FILE
+
+log_file="---------------------------------- finish encrypting files in $1 ----------------------------------"
+echo $log_file
+echo $log_file >> $LOG_FILE
 fi 
 
 #------------------------Decryption--------------------------------
 if [ $3 = "decrypt" ]; then
-echo "---------------------------------- start decrypting files in $1 ----------------------------------"
-echo "---------------------------------- start decrypting files in $1 ----------------------------------" >> $LOG_FILE
+
+log_file="---------------------------------- start decrypting files in $1 ----------------------------------"
+echo $log_file
+echo $log_file >> $LOG_FILE
 echo 
 echo  >> $LOG_FILE
 counter=0  
 start=$SECONDS
+
 find $1 -not -path '*/\.*' -type f \( ! -iname '.*' \) -print0 | while read -d $'\0' file; do
 	if [ ${file: -4} = ".enc" ] && [ -f $2 ]; then
-		echo "decrypting $file ..." 
-		echo "decrypting $file ..." >> $LOG_FILE
+		log_file="decrypting $file ..."
+		echo $log_file
+		echo $log_file >> $LOG_FILE
 
 		if $( openssl aes-256-cbc -d -in "$file" -out "${file/%????}" -pass file:$2 ); then 
 		    openssl aes-256-cbc -d -in "$file" -out "${file/%????}" -pass file:$2
-		    echo "deleting $file ..."
-		    echo "deleting $file ..." >> $LOG_FILE
+
+		    log_file="deleting $file ..."
+		    echo $log_file
+		    echo $log_file >> $LOG_FILE
 		    rm "$file"
 		    let counter++
 		    end=$SECONDS
-		    echo "$counter files have been decrypted in $((end-start)) seconds"
-		    echo "$counter files have been decrypted in $((end-start)) seconds" >> $LOG_FILE
+
+		    log_file="$counter files have been decrypted in $((end-start)) seconds"
+		    echo $log_file
+		    echo $log_file >> $LOG_FILE
 		    echo
 		    echo >> $LOG_FILE
 		else
 			rm "${file/%????}"
+
+			log_file="your files can't be decrypted!"
 			echo 
-		    echo "damn, bad password! Your files can't be decrypted ..."
+		    echo $log_file
 		    echo 
-	    	echo "damn, bad password! Your files can't be decrypted ..." >> $LOG_FILE
+	    	echo $log_file >> $LOG_FILE
+	    	break 
 	    fi 	
 	else 
 		if [ ! -f $2 ]; then 
-		    echo "password file doesn't exit ..."
-	    	echo "password file doesn't exit ..." >> $LOG_FILE
-    	fi 
+			log_file="password file doesn't exit!"
+		    echo $log_file
+	    	echo $log_file >> $LOG_FILE
+	    	break 
+    	fi
 	fi 
 done
-echo "---------------------------------- finish decrypting files in $1 ----------------------------------"
-echo "---------------------------------- finish decrypting files in $1 ----------------------------------" >> $LOG_FILE
+
+log_file="---------------------------------- finish decrypting files in $1 ----------------------------------"
+echo $log_file
+echo $log_file >> $LOG_FILE
 fi
 echo 
 echo  >> $LOG_FILE
